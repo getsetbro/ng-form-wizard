12dd15dbb4027bcaaff85ceb72046a941cc837d3
sethb <getsetbro@users.noreply.github.com>
2020-06-14 23:25:52 -0400

    animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,51 @@
 import { Component } from '@angular/core';
+import {
+  transition,
+  trigger,
+  state,
+  style,
+  animate,
+  query,
+  animateChild,
+  group,
+} from '@angular/animations';
+
 
 @Component({
   selector: 'app-root',
   template: `<div class="p-8 m-8">
-      <span>Welcome to {{title}}!</span>
       <button routerLink="" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Home</button>
       <button routerLink="/survey" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Survey</button>
-    <router-outlet></router-outlet>
+      <main [@routeAnimation]="o.isActivated ? o.activatedRoute : ''">
+        <router-outlet #o="outlet"></router-outlet>
+      </main>
     </div>
   `,
-  styles: []
+  styles: [],
+  animations: [
+    trigger('routeAnimation', [
+      transition('* <=> *', [
+        style({ position: 'relative' }),
+        query(':enter, :leave', [
+          style({
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%'
+          })
+        ]),
+        query(':enter', [style({ left: '-100%' })]),
+        query(':leave', animateChild()),
+        group([
+          query(':leave', [animate('200ms ease-out', style({ left: '100%' }))]),
+          query(':enter', [animate('300ms ease-out', style({ left: '0%' }))])
+        ]),
+        query(':enter', animateChild()),
+      ])
+    ]
+    )]
 })
 export class AppComponent {
   title = 'fwizapp';
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { NgxSpinnerModule } from "ngx-spinner";
@@ -32,6 +33,7 @@ import { MultipleChoiceComponent } from './components/survey/multiple-choice/mul
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
     NgxSpinnerModule,
   ],
   providers: [],
